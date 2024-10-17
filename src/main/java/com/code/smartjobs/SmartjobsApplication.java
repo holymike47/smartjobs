4231ac9471b8c4c9d4a9b5feeb95327695bb9b78
Holymike47 <holymike47@yahoo.com>
2024-10-17 18:15:55 -0400

    initial commit

---
+++ src/main/java/com/code/smartjobs/SmartjobsApplication.java
@@ -0,0 +1,13 @@
+package com.code.smartjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartjobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/smartjobs/SmartjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.smartjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
